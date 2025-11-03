a50d8c0bba3e812725a5d9edae8753a7a1fc0f85
Nihal-dev-code <nihalmakandar402@gmail.com>
2025-11-03 18:11:41 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID=1234
+const accountemail="nihal@gmail.com"
+const accountpassword="3456"
+accountcity="jaipur"
+let accountstate;
+
+// accountID=2
+console.table([accountID, accountemail, accountpassword, accountcity, accountstate]);
