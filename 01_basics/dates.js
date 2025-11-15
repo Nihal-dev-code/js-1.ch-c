c081c0a3a03b8564b41fe584f84191fdb2e381b2
Nihal-dev-code <nihalmakandar402@gmail.com>
2025-11-15 07:52:43 +0000

    Add dates.js with date manipulation examples

---
+++ 01_basics/dates.js
@@ -0,0 +1,12 @@
+let mydate=new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+console.log(typeof mydate);
+
+let mycreatedate=new Date(2023,8,24)
+console.log(mycreatedate.toDateString());
+
+let newdate=new Date()
+console.log(newdate.getMonth());
