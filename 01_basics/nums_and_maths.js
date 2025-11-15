7022ed679ea53470d9069265392a59ed4732c61d
Nihal-dev-code <nihalmakandar402@gmail.com>
2025-11-15 07:49:16 +0000

    dates

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,20 @@
+// numbers
+const score=400
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+const othernumber=123.89666
+console.log(othernumber.toPrecision(4));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
+
+// maths
+console.log(Math.random());
+console.log(Math.random()*1+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
