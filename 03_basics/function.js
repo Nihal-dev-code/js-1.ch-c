6f89c8c00a4a33ef961ffa8debb7edac409d18fb
Nihal-dev-code <nihalmakandar402@gmail.com>
2025-11-17 08:04:59 +0000

    function

---
+++ 03_basics/function.js
@@ -0,0 +1,24 @@
+function saymyname(){
+    console.log("n");
+    console.log("i");
+    console.log("h");
+    console.log("a");
+    console.log("l");
+}
+// saymyname()
+function addtwonumbers(number1,number2){
+    console.log(number1+number2);
+}
+addtwonumbers()   //arguments
+addtwonumbers(3,7)
+
+// parameter
+function loginusermessage(username){
+    if(!username){
+        console.log("enter a username");
+        
+
+    }
+    return `${username} just logged in`
+}
+console.log(loginusermessage());
