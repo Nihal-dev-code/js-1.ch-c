c740a5f15d5c279848a99794d2754d631ff49a18
Nihal-dev-code <nihalmakandar402@gmail.com>
2025-11-26 13:58:15 +0000

    for

---
+++ iteration/one.js
@@ -0,0 +1,24 @@
+// for
+
+// for(let i=0;i<=10; i++){
+//     console.log(`outer loop value: ${i}`);
+//     for(let j=0; j <=10; j++){
+//         // const i=Array.length
+//         // console.log(`outer loop value ${j} and inner loop value ${i}`);
+//         console.log(i+'*'+j+'='+i*j);
+        
+//     }
+    
+// }
+
+// break,continue
+
+for(let index=1; index<=20; index++)
+{
+    if(index==5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`index value is ${index}`);
+    
+}
