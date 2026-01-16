87af07d323e00b48c283492f72e128b9c9e423b4
Nihal-dev-code <nihalmakandar402@gmail.com>
2026-01-16 15:04:10 +0000

    a

---
+++ iteration/seven.js
@@ -0,0 +1,9 @@
+const mynumers=[1,2,3,4,5,6,7,8,9,10]
+   const  mynums=mynumers.map((num)=>{return num+10})
+    console.log(mynums);
+    console.log(mynumers);
+    
+    if((mynumers[1])==1){
+     console.log(num+12);
+    }
+    
